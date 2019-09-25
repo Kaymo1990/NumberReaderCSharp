b5b804bcc5e353937c4e71903be2af13bde00664
Kay <Kay@LAPTOP-R26J0GK6>
2019-09-25 23:47:24 +0100

    updated number reader class

---
+++ NumberReader/NumberReader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumberReader
+{
+    class NumberReader
+    {
+        public static void IsValidNumber(int testNumber)
+        {
+            if ((testNumber <= 10) && (testNumber >= 1))
+            {
+                Console.WriteLine("Valid");
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
---
+++ NumberReader/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NumberReader;
+
+namespace NumberReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number between 1 and 10");
+            var numberInput = Console.ReadLine();
+
+            NumberReader.IsValidNumber(int.Parse(numberInput));
+        }
+    }
+}
